3d6b157885d4273dc7fd9f31a283732be397d11c
SHEIK SADIQ <sheiksadiq8099@outlook.com>
2025-05-07 15:41:45 +0530

    Update tag_resources.py

--- tag_resources.py
+++ tag_resources.py
@@ -1,5 +1,5 @@
 import boto3
-from resource_arns import resource_arn, s3_resource_arns, dynamodb_resource_arns
+from resource_arns import resource_arns
 # Initialize tagging client
 client = boto3.client('resourcegroupstaggingapi')
 
