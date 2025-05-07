f11da1b01ebf539dade7a6a2f4ae792f328d7c19
SHEIK SADIQ <sheiksadiq8099@outlook.com>
2025-05-07 15:09:58 +0530

    Create resource_arns.py

---
+++ resource_arns.py
@@ -0,0 +1,26 @@
+dynamodb_resource_arns = [
+    'arn:aws:dynamodb:us-east-1:131332286832:table/test',
+]
+
+s3_resource_arns = [
+    'arn:aws:s3:::my-logs-bucket-11111',
+    'arn:aws:s3:::mydatabucket-111111'
+]
+
+kafka_resource_arns = [
+
+] # ARN formate: 'arn:aws:kafka:region:account-id:cluster/cluster-name/cluster-id'
+
+opensearch_resource_arns = [
+
+] # ARN formate: 'arn:aws:es:region:account-id:domain/domain-name'
+
+rds_resource_arns = [
+
+]  # ARN formate: 'arn:aws:rds:region:account-id:cluster/cluster-name'
+
+
+
+resource_arn = [
+    'arn:aws:ec2:us-east-1:131332286832:instance/i-054ddc96852c95823'
+]
